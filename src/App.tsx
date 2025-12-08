ef695376496285f7a1b2c54e4ccd8cce4bf213ef
somilany <bordyukovadascha@gmail.com>
2025-12-08 16:47:59 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  // SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+  SidebarTrigger
+} from "@/components/ui/sidebar"
+
+function App() {
+  return (
+    <SidebarProvider>
+      <Sidebar>
+        <SidebarTrigger />
+        <SidebarContent>
+          <SidebarGroup>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                <SidebarMenuItem>
+                  <SidebarMenuButton>Item One</SidebarMenuButton>
+                </SidebarMenuItem>
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        </SidebarContent>
+      </Sidebar>
+    </SidebarProvider>
+  )
+}
+
+export default App
