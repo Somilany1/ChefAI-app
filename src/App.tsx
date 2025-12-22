7713df9c984fc4575640c956c06622461173d136
somilany <bordyukovadascha@gmail.com>
2025-12-22 20:33:53 +0300

    Create main content on home page

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import {
 } from "@/components/ui/sidebar"
 import { AppSidebar } from "@/components/ui/app-sidebar"
 import { AppBreadcrumb } from "@/components/ui/app-breadcrumb"
+import { AppCarousel } from "./components/ui/app-carousel"
 
 function App() {
   return (
@@ -15,9 +16,20 @@ function App() {
           <SidebarTrigger className="-ml-1" />
           <AppBreadcrumb />
         </header>
-        <div></div>
-        <div></div>
-        <div></div>
+        <div className="grid grid-rows-3 gap-4 m-4 h-full">
+          <div className="bg-gray-50 rounded-2xl">
+            <span className="m-8">Breakfast Recommendations</span>
+            <AppCarousel></AppCarousel>
+          </div>
+          <div className="bg-gray-50 rounded-2xl">
+            <span>Lunch Recommendations</span>
+            <AppCarousel></AppCarousel>
+          </div>
+          <div className="bg-gray-50 rounded-2xl">
+            <span>Dinner Recommendations</span>
+            <AppCarousel></AppCarousel>
+          </div>
+        </div>
       </SidebarInset>
     </SidebarProvider>
   )
--- src/components/ui/app-carousel.tsx
+++ src/components/ui/app-carousel.tsx
@@ -1,17 +1,30 @@
 import {
-    Carousel,
-    CarouselContent,
-    CarouselItem,
-    CarouselNext,
-    CarouselPrevious,
-  } from "@/components/ui/carousel"
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
 
-  <Carousel>
-  <CarouselContent>
-    <CarouselItem>...</CarouselItem>
-    <CarouselItem>...</CarouselItem>
-    <CarouselItem>...</CarouselItem>
-  </CarouselContent>
-  <CarouselPrevious />
-  <CarouselNext />
-</Carousel>
+import {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardAction,
+  CardDescription,
+  CardContent,
+} from "@/components/ui/card"
+
+export function AppCarousel() {
+  return (
+    <Carousel className="h-0">
+      <CarouselContent>
+        <CarouselItem className="basis-1/4">First Recipe</CarouselItem>
+        <CarouselItem className="basis-1/4">Second Recipe</CarouselItem>
+        <CarouselItem className="basis-1/4">Third Recipe</CarouselItem>
+        <CarouselItem className="basis-1/4">Fourth Recipe</CarouselItem>
+      </CarouselContent>
+    </Carousel>
+  )
+}
